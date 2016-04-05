956a2273587ff050a99fe37c109bd603db42f502
hemuinakollu <hemuinakollu@missionrnd.com>
2016-04-05 20:08:43 +0530

    Submit Commit

--- src/BSTTransversals.cpp
+++ src/BSTTransversals.cpp
@@ -9,7 +9,7 @@ Note : Memory for the Array is already allocated ,Dont allocate it again using m
 ->Take help of helper functions which are actually solving the problem .
 ->Dont Process Invalid Inputs
 
-Bonus Task : 
+Bonus Task :
 ->Go to the BSTTransversals Spec File ,We have left a custom test case for you ,Try to fill
 it and understand how testing works .
 */
@@ -23,13 +23,73 @@ struct node{
 };
 
 
+void ino(struct node *root, int *arr, int *i)
+{
+	if (root != NULL)
+	{
+		ino(root->left, arr, i);
+		arr[*i] = root->data;
+		(*i)++;
+		ino(root->right, arr, i);
+	}
+	else
+		return;
+
+}
 void inorder(struct node *root, int *arr){
-	
+	if (root == NULL || arr == NULL)
+	{
+		return;
+	}
+	int i = 0;
+	ino(root, arr, &i);//passing &i as call by reference to update the i value
+
+
+}
+void preo(struct node *root, int *arr, int *i)
+{
+	if (root != NULL)
+	{
+		arr[*i] = root->data;
+		(*i)++;
+		preo(root->left, arr, i);
+		preo(root->right, arr, i);
+	}
+	else
+		return;
+
 }
 void preorder(struct node *root, int *arr){
-	
+	if (root == NULL || arr == NULL)
+	{
+		return;
+
+	}
+	int i = 0;
+	preo(root, arr, &i);
+
+
+}
+void posto(struct node *root, int *arr, int *i)
+{
+	if (root != NULL)
+	{
+		posto(root->left, arr, i);
+		posto(root->right, arr, i);
+		arr[*i] = root->data;
+		(*i)++;
+	}
+	else
+		return;
+
 }
 void postorder(struct node *root, int *arr){
-	
+	if (root == NULL || arr == NULL)
+	{
+		return;
+	}
+	int  i = 0;
+	posto(root, arr, &i);
+
 }
 
