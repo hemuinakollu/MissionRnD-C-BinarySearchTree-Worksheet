164ed94e247dac533069ca587161b496a4376bd1
hemuinakollu <hemuinakollu@missionrnd.com>
2016-04-06 22:11:29 +0530

    Submit Commit

--- src/BSTRows.cpp
+++ src/BSTRows.cpp
@@ -22,16 +22,63 @@ Note : Return -1 for Invalid Cases .
 
 #include <stdlib.h>
 #include <stdio.h>
-
+#include<malloc.h>
 struct node{
 	struct node * left;
 	int data;
 	struct node *right;
 };
 
+int get_height1(struct node *root){
 
+	int max = 0;
+	if (root == NULL)
+		return 0;
+	else
+	{
+		int l = get_height1(root->left);
+		int r = get_height1(root->right);
 
+		if (l > r)
+			max = l + 1;
+		else
+			max = r + 1;
+		return max;
+	}
+
+}
+
+void right_to_left(struct node* root, int l,int *j,int *a)
+{
+	if (root == NULL)
+		return;
+	if (l == 1)
+	{
+		a[*j] = root->data;
+		(*j)++;
+	}
+	else if (l > 1)
+	{
+		right_to_left(root->right, l - 1,j,a);
+		right_to_left(root->left, l - 1,j,a);
+	}
+}
 int* BSTRighttoLeftRows(struct node* root)
 {
-    return NULL;
+	int *a=(int*)malloc(100);
+	if (root == NULL)
+		return NULL;
+	else
+	{
+		int l = get_height1(root);
+		int i;
+		int j = 0;
+		
+		for (i = 1; i <= l; i++)
+			right_to_left(root, i, &j, a);
+		return a;
+		
+	}
+
+   
 }
